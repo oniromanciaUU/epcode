8c96cca943595e5d525441b68e828c4dc0d1e10a
oniromanciaUU <patrick.carroll@ub.uu.se>
2024-03-07 16:14:32 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,16 +6,15 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Copy the below text</title>
+        <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <Header title="Copy the below text" />
         <p className="description">
-    rundll32 printui.dll,PrintUIEntry /y /in /n \\edp-uu-prn01.user.uu.se\eduPrint-UU
-      
-<! --    Get started by editing <code>pages/index.js</code> -->
+          rundll32 printui.dll,PrintUIEntry /y /in /n \\edp-uu-prn01.user.uu.se\eduPrint-UU
+
         </p>
       </main>
 
